2dfa6382bcd0ac5f8b0b7bcb2b4022017502c8e0
HIT137S371117 <nguyen.james698@gmail.com>
2024-09-14 10:08:58 +0930

    Task1 Completed

---
+++ Question1_Task1.py
@@ -0,0 +1,14 @@
+import os, csv #Operating System and Comma Separated Values (Allow to read csv files)
+csv_D=r'C:\Study\WINT01 Bachelor of IT\First Year Sem2\HIT137 Software now\Assignment 2' #Change this to your file location 
+
+#It will create another text file and posted within the folder of your selected folder of the script you have created ^
+output_file = 'output.txt'
+
+with open(output_file, 'w', encoding='utf-8') as txt_file: 
+    for csv_filename in os.listdir(csv_D):
+        if csv_filename.endswith('.csv'):
+            csv_path=os.path.join(csv_D, csv_filename)
+            with open(csv_path, 'r', encoding='utf-8') as csv_file:
+                reader=csv.reader(csv_file) 
+                for row in reader:
+                    txt_file.write(' '.join(row)+'\n')
